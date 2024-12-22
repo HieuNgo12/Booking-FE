16017538df1699e80a1bef506c41f4025acfef69
HieuNgo12 <cusaniv@gmail.com>
2024-12-22 18:22:19 +0700

    fix

---
+++ src/pages/AccountPage/EditIDCardPage.jsx
@@ -0,0 +1,343 @@
+import React, { useEffect, useState } from "react";
+import {
+  Form,
+  Input,
+  Button,
+  Typography,
+  DatePicker,
+  Upload,
+  message,
+  Row,
+  Col,
+} from "antd";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+import ImgCrop from "antd-img-crop";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-phone-input-2/lib/style.css";
+import moment from "moment";
+
+const EditIDCardPage = ({ dataUser }) => {
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
+  const [frontFileList, setFrontFileList] = useState([]);
+  const [backFileList, setBackFileList] = useState([]);
+  const [changeBtn, setChangeBtn] = useState(true);
+  const navigate = useNavigate();
+
+  const beforeUpload = (file) => {
+    const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+    if (!isJpgOrPng) {
+      message.error("You can only upload JPG/PNG file!");
+      return Upload.LIST_IGNORE;
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error("Image must smaller than 2MB!");
+      return Upload.LIST_IGNORE;
+    }
+    return true;
+  };
+
+  const handleFrontChange = ({ fileList }) => {
+    setFrontFileList(fileList.slice(-1));
+  };
+
+  const handleBackChange = ({ fileList }) => {
+    setBackFileList(fileList.slice(-1));
+  };
+
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
+
+  const handleNameUpperCase = (e) => {
+    const value = e.target.value.toUpperCase();
+    form.setFieldsValue({ fullName: value });
+  };
+
+  const onFinish = async (values) => {
+    console.log("Form Values: ", values);
+    try {
+      let req1 = await fetch(`${import.meta.env.VITE_URL_API}/update-id-card`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({
+          number: values.number,
+          fullName: values.fullName,
+          DOI: values.DOI,
+          POI: values.POI,
+        }),
+      });
+      if (req1.status === 401) {
+        const req2 = await fetch(
+          `${import.meta.env.VITE_URL_API}/refresh-token`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+          }
+        );
+        if (req2.ok) {
+          let req1 = await fetch(
+            `${import.meta.env.VITE_URL_API}/update-id-card`,
+            {
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              credentials: "include",
+              body: JSON.stringify({
+                number: values.number,
+                fullName: values.fullName,
+                DOI: values.DOI,
+                POI: values.POI,
+              }),
+            }
+          );
+          let res1 = await req1.json();
+          if (req1.ok) {
+            toast.success(res1.message, {
+              position: "top-center",
+              autoClose: 1000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+              onClose: () => setChangeBtn(true),
+            });
+          } else if (req1.status === 400) {
+            toast.warn(res1.message, {
+              position: "top-center",
+              autoClose: 1000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+            });
+          }
+        }
+      }
+      let res1 = await req1.json();
+      if (req1.ok) {
+        toast.success(res1.message, {
+          position: "top-center",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          onClose: () => setChangeBtn(true),
+        });
+      } else if (req1.status === 400) {
+        toast.warn(res1.message, {
+          position: "top-center",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Error internal", {
+        position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
+
+  return (
+    <div className="p-6 w-3/4 mx-auto bg-white rounded-lg shadow-md">
+      <div className="flex justify-between items-center">
+        <Typography.Title level={2}>Edit ID Card</Typography.Title>
+        <Button
+          type="default"
+          onClick={() => navigate(-1)}
+          style={{ marginBottom: "20px" }}
+        >
+          Back
+        </Button>
+      </div>
+
+      <p className="text-gray-600 mb-5">
+        Please upload your ID card information to keep your account secure.
+      </p>
+
+      {dataUser ? (
+        <Form
+          form={form}
+          layout="vertical"
+          className="mt-5"
+          onFinish={onFinish}
+          initialValues={{
+            number: dataUser?.idCard.number || "",
+            DOI: dataUser?.idCard.DOI ? moment(dataUser.idCard.DOI) : null,
+            fullName: dataUser?.idCard.fullName || "",
+            POI: dataUser?.idCard.POI || "",
+          }}
+        >
+          {/* First Row */}
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name="number"
+                label="ID Card"
+                rules={[{ required: true, message: "ID Card is required" }]}
+              >
+                <Input
+                  placeholder="Enter ID Card Number"
+                  disabled={changeBtn}
+                />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name="DOI"
+                label="Date of Issue"
+                rules={[
+                  { required: true, message: "Date of Issue is required" },
+                ]}
+              >
+                <DatePicker
+                  style={{ width: "100%" }}
+                  placeholder="Select Date"
+                  disabled={changeBtn}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          {/* Second Row */}
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name="fullName"
+                label="Full Name"
+                rules={[{ required: true, message: "Full Name is required" }]}
+              >
+                <Input
+                  placeholder="Enter Full Name"
+                  disabled={changeBtn}
+                  onChange={handleNameUpperCase}
+                />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name="POI"
+                label="Place of Issue"
+                rules={[
+                  { required: true, message: "Place of Issue is required" },
+                ]}
+              >
+                <Input
+                  placeholder="Enter Place of Issue"
+                  disabled={changeBtn}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          {/* Image Upload Section */}
+          <div className="mt-5">
+            <Row gutter={16}>
+              <Col span={12}>
+                <div className="font-bold mb-2">Front ID Card</div>
+                <Form.Item>
+                  <ImgCrop rotate aspect={85.6 / 54} grid>
+                    <Upload
+                      name="frontId"
+                      listType="picture-card"
+                      fileList={frontFileList}
+                      beforeUpload={beforeUpload}
+                      onChange={handleFrontChange}
+                    >
+                      {frontFileList.length < 1 && uploadButton}
+                    </Upload>
+                  </ImgCrop>
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <div className="font-bold mb-2">Back ID Card</div>
+                <Form.Item>
+                  <ImgCrop rotate aspect={85.6 / 54} grid>
+                    <Upload
+                      name="backId"
+                      listType="picture-card"
+                      fileList={backFileList}
+                      beforeUpload={beforeUpload}
+                      onChange={handleBackChange}
+                    >
+                      {backFileList.length < 1 && uploadButton}
+                    </Upload>
+                  </ImgCrop>
+                </Form.Item>
+              </Col>
+            </Row>
+          </div>
+
+          {/* Submit Button */}
+          <Form.Item>
+            {changeBtn ? (
+              <Button
+                type="primary"
+                htmlType="button"
+                className="w-full bg-[#07689F]"
+                style={{
+                  whiteSpace: "nowrap",
+                }}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setChangeBtn(false);
+                }}
+              >
+                CHANGE
+              </Button>
+            ) : (
+              <Button
+                htmlType="submit"
+                type="primary"
+                className="w-full"
+                style={{
+                  whiteSpace: "nowrap",
+                  backgroundColor: "green",
+                }}
+              >
+                SAVE
+              </Button>
+            )}
+          </Form.Item>
+        </Form>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default EditIDCardPage;
---
+++ src/pages/AccountPage/SideBarAccountPage.jsx
@@ -0,0 +1,152 @@
+import React from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import {
+  UserOutlined,
+  CalendarOutlined,
+  FolderOpenOutlined,
+  QuestionCircleOutlined,
+  SettingOutlined,
+  RightOutlined,
+  KeyOutlined,
+  ApiOutlined,
+  StopOutlined,
+  ProfileOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+import Cookies from "js-cookie";
+
+const list = [
+  { name: "Profile", value: "profile", icon: UserOutlined },
+  { name: "Authentication", value: "authentication", icon: KeyOutlined },
+  { name: "Booking", value: "booking", icon: CalendarOutlined },
+  { name: "Preferences", value: "preferences", icon: ProfileOutlined },
+  { name: "Payment", value: "payment", icon: FolderOpenOutlined },
+  { name: "Support", value: "support", icon: QuestionCircleOutlined },
+  { name: "Setting", value: "setting", icon: SettingOutlined },
+];
+
+const SideBarAccountPage = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const navigate = useNavigate();
+
+  const Logout = () => {
+    Cookies.remove("accessToken");
+    Cookies.remove("refreshToken");
+    Cookies.remove("refreshToken");
+    navigate("/login");
+  };
+  return (
+    <div
+      style={{
+        width: "25%",
+        height: "50%",
+        paddingRight: "20px",
+        borderRight: "1px solid #ccc",
+        backgroundColor: "#fff",
+        borderRadius: "8px",
+        padding: "20px",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      <ul style={{ listStyleType: "none", padding: 0, margin: 0 }}>
+        {list.map((item, index) => {
+          const isActive = currentPath === `/account/${item.value}`;
+          return (
+            <li
+              key={index}
+              style={{
+                marginBottom: "15px",
+                fontFamily: "Poppins",
+              }}
+            >
+              <Link
+                to={`/account/${item.value}`}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  padding: "12px 16px",
+                  textDecoration: "none",
+                  fontSize: "16px",
+                  color: isActive ? "#07689F" : "#000",
+                  border: "1px solid #eaeaea",
+                  borderRadius: "8px",
+                  boxShadow: isActive
+                    ? "0px 2px 4px rgba(0, 0, 0, 0.1)"
+                    : "none",
+                  backgroundColor: isActive ? "#f0f9ff" : "#fff",
+                  transition: "all 0.3s",
+                }}
+                onMouseEnter={(e) => {
+                  e.target.style.backgroundColor = "#f5f5f5";
+                }}
+                onMouseLeave={(e) => {
+                  e.target.style.backgroundColor = isActive
+                    ? "#f0f9ff"
+                    : "#fff";
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "8px",
+                  }}
+                >
+                  <item.icon style={{ fontSize: "18px" }} />
+                  <span>{item.name}</span>
+                </div>
+                <RightOutlined />
+              </Link>
+            </li>
+          );
+        })}
+        <li
+          style={{
+            marginBottom: "15px",
+            fontFamily: "Poppins",
+            borderTop: "1px solid #eaeaea", // Đường kẻ
+            paddingTop: "15px", // Khoảng cách trên đường kẻ
+            cursor: "pointer",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              padding: "12px 16px",
+              textDecoration: "none",
+              fontSize: "16px",
+              border: "1px solid #eaeaea",
+              borderRadius: "8px",
+              transition: "all 0.3s",
+            }}
+            onMouseEnter={(e) => {
+              e.target.style.backgroundColor = "#f5f5f5";
+            }}
+            onMouseLeave={(e) => {
+              e.target.style.backgroundColor = "#fff";
+            }}
+            onClick={() => Logout()}
+          >
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "8px",
+              }}
+            >
+              <LogoutOutlined style={{ fontSize: "18px" }} />
+              <span>Log Out</span>
+            </div>
+            <RightOutlined />
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideBarAccountPage;
